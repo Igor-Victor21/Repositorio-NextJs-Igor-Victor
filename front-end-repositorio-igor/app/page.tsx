4ec7cef603fa8feed998c3ca46eb89cae422c459
Igor-Victor21 <igor.victorcontato@gmail.com>
2025-12-17 19:23:35 -0300

    Iniciando section projetos

--- front-end-repositorio-igor/app/page.tsx
+++ front-end-repositorio-igor/app/page.tsx
@@ -9,6 +9,8 @@ import IconGithub from "./assets/IconGithub.png"
 import IconWhatsApp from "./assets/IconWhatsapp.png"
 import IconCurriculo from "./assets/IconCurriculo.png"
 import IconDeveloper from "./assets/IconDeveloper.png"
+import EcommerceImage from "./assets/E-commerceProject.png"
+
 
 
 
@@ -26,6 +28,8 @@ export default function Home() {
   return (
     <>
       <Navbar />
+
+      {/* Container Text */}
       <section id="inicio" className="scroll-mt-[87px] flex w-full h-full min-h-screen bg-home-gradient">
         <div className="flex flex-col w-full h-auto px-4 my-[143px] bg-transparent md:max-w-[1123px] md:mx-[101px] md:my-[143px] md:px-0">
           <p className="text-[#F5F5F5] text-[20px]">
@@ -61,6 +65,8 @@ export default function Home() {
           </p>
           <br />
           <br />
+
+          {/* Container Buttons */}
           <div className="flex flex-col gap-[40px] md:flex-row">
             <div className="flex p-2 rounded-[50px] bg-[#F5F5F5] w-[252px] h-[56px] items-center justify-center">
               <button onClick={handleZap} className="flex gap-1 text-[#121212] font-medium cursor-pointer hover:tracking-wide duration-200">VAMOS CONVERSAR <Image src={IconWhatsApp} alt="IconWhatsApp" width={24} height={24} /></button>
@@ -68,6 +74,8 @@ export default function Home() {
             <div className="flex p-2 rounded-[50px] bg-[#F5F5F5] w-[231px] h-[56px] items-center justify-center ">
               <button onClick={() => window.open("/Curriculo-Igor-Victor.pdf", "_blank")} className="flex gap-1 text-[#121212] font-medium cursor-pointer hover:tracking-wide duration-200">VER CURRÍCULO <Image src={IconCurriculo} alt="IconWhatsApp" width={24} height={24} /></button>
             </div>
+
+            {/* Social media */}
             <div className="flex md:bottom-[15px] md:right-[20px] md:absolute">
               <Link href="https://www.linkedin.com/in/igor-victor21/" target="_blank" rel="noopener noreferrer" className="mr-[32px]">
                 <Image src={IconLinkedin} alt="linkedin" width={45} height={45} />
@@ -78,16 +86,60 @@ export default function Home() {
             </div>
           </div>
         </div>
+
+        {/* Icon dev */}
         <div className="md:float-left md:mr-57 md:w-[401.47px] md:h-[348.65px]">
           <Image src={IconDeveloper} alt="Developer" className="hidden md:block md:absolute md:top-[183px] object-contain" />
         </div>
       </section>
 
-
+      {/* Section Project */}
       <section id="projetos" className=" scroll-mt-[87px] w-full h-full bg-pages-gradient">
-        {/* Título página */}
+
+        {/* Title page */}
         <div className="flex items-center justify-center w-full h-[87px] border-b-3 border-[#F5F5F5] border-t-3 border-[#F5F5F5]">
           <h1 className="text-[#F5F5F5] text-center font-bold text-[40px] border-b-2 border-[#F5F5F5]">Projetos</h1>
+        </div>
+
+        {/* Container Project Cards  */}
+        <div className="flex flex-col items-center justify-center mt-[20px] md:flex-row">
+          {/* Cards 01 */}
+          <div className="flex flex-col items-center w-[350px] h-[648px] bg-[#3A3A3A] m-[15px] rounded-[16px] ">
+            <div className="m-[15px]">
+              <h2 className="text-center text-[#F5F5F5] font-bold text-[18px]">E-COMMERCE</h2>
+              <span className="text-[#F5F5F5] border-b-[1.2px] border-[#F5F5F5] text-[14px]">PROJETO PARA VC BRINQUEDOS ESPUMADOS</span>
+            </div>
+            {/* Image */}
+              <Image src={EcommerceImage} alt="ImageProject" className="w-[95%] ml-[15px]"/>
+            <hr className="border-[1.5px] border-[#F5F5F5] w-[90%]"/>
+          </div>
+
+          {/* Cards 02 */}
+          <div className="flex flex-col items-center w-[350px] h-[648px] bg-[#3A3A3A] m-[15px] rounded-[16px] ">
+            <div className="m-[15px]">
+              <h2 className="text-center text-[#F5F5F5] font-bold text-[18px]">PAINEL ADMINISTRATIVO</h2>
+              <span className="text-[#F5F5F5] border-b-[1.2px] border-[#F5F5F5] text-[14px]">PROJETO PARA VC BRINQUEDOS ESPUMADOS</span>
+            </div>
+            {/* Image */}
+              <Image src={EcommerceImage} alt="ImageProject" className="w-[95%] ml-[15px]"/>
+            <hr className="border-[1.5px] border-[#F5F5F5] w-[90%]"/>
+          </div>
+
+          {/* Cards 03 */}
+          <div className="flex flex-col items-center w-[350px] h-[648px] bg-[#3A3A3A] m-[15px] rounded-[16px] ">
+            <div className="m-[15px]">
+              <h2 className="text-center text-[#F5F5F5] font-bold text-[18px]">APP INTERNO - MOBILE</h2>
+              <span className="text-[#F5F5F5] border-b-[1.2px] border-[#F5F5F5] text-[14px]">PROJETO PARA VC BRINQUEDOS ESPUMADOS</span>
+            </div>
+            {/* Image */}
+              <Image src={EcommerceImage} alt="ImageProject" className="w-[95%] ml-[15px]"/>
+            <hr className="border-[1.5px] border-[#F5F5F5] w-[90%]"/>
+          </div>
+        </div>
+
+        {/* Container button see more */}
+        <div>
+          <button>VER MAIS</button>
         </div>
       </section>
 
