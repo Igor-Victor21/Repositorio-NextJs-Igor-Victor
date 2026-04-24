6cba65c421d4a57558c6798ca67bef3f4bc6723a
Igor-Victor21 <igor.victorcontato@gmail.com>
2026-04-24 11:39:58 -0300

    Atualizando o dominio dos projetos

--- front-end-repositorio-igor/app/components/buttonsProject.tsx
+++ front-end-repositorio-igor/app/components/buttonsProject.tsx
@@ -23,15 +23,15 @@ export default function ButtonsProject() {
         <section>
             <div className="flex flex-row gap-[10px] md:mb-[20px]">
                 <div className="flex p-2 rounded-[50px] bg-[#F5F5F5] w-[128.76px] md:w-[168.76px] h-[46px] items-center justify-center">
-                    <Link href="https://vc-brinquedos-espumados-site-next.vercel.app/" target="_blank" rel="noopener noreferrer" className="flex gap-1 text-[#121212] font-medium cursor-pointer hover:tracking-wide duration-200">SITE
-                        <Image src={IconLink} alt="IconWhatsApp" width={24} height={24} />
+                    <Link href="https://www.vcbrinquedosespumados.com.br/" target="_blank" rel="noopener noreferrer" className="flex gap-1 text-[#121212] font-medium cursor-pointer hover:tracking-wide duration-200">SITE
+                        <Image src={IconLink} alt="IconLink" width={24} height={24} />
                     </Link>
                 </div>
 
                 
                 <div className="flex p-2 rounded-[50px] bg-[#F5F5F5] w-[184.11px] h-[46px] items-center justify-center ">
                     <Link href="https://github.com/Igor-Victor21/Documentacao-VC-Brinquedos-Espumados" target="_blank" rel="noopener noreferrer" className="flex gap-1 text-[#121212] font-medium cursor-pointer hover:tracking-wide duration-200">DOCUMENTAÇÃO
-                        <Image src={IconEye} alt="IconWhatsApp" width={24} height={24} />
+                        <Image src={IconEye} alt="IconDoc" width={24} height={24} />
                     </Link>
 
                 </div>
